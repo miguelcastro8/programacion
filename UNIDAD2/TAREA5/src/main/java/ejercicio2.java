fe35227f706e1a9ac3178df79970c150c8429fd1
Darioeab27 <alum.denriqueacevedob.2020@iesalixar.org>
2020-10-19 16:24:05 +0200

    ejercicio2 tarea5 tema2 java

---
+++ UNIDAD2/TAREA5/src/main/java/ejercicio2.java
@@ -0,0 +1,13 @@
+class ejercicio2 {
+
+    public static void main(String args[]) {
+
+        int numero = 12;
+        if (numero == 12) {
+            System.out.println("Ha introducido el numero correctamente");
+        }
+        else {
+            System.out.println("No ha introducido el numero correctamente");
+        }
+    }
+}
