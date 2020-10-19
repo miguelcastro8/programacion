91a7194170d7fb9ff19b3e27f298ba084e1648ca
Darioeab27 <alum.denriqueacevedob.2020@iesalixar.org>
2020-10-19 17:18:03 +0200

    ejercicio4 tarea5 tema2 java

---
+++ UNIDAD2/TAREA5/src/main/java/ejercicio4.java
@@ -0,0 +1,25 @@
+class ejercicio4 {
+
+    public static void main (String args[]){
+
+        System.out.println("Dame un numero entero");
+        int entero1 = 2;
+
+        if (entero1/2==0){
+            System.out.println("el primer numero es par");
+        }
+        else{
+            System.out.println("El primer numero es par");
+        }
+
+        System.out.println("Dame otro numero entero");
+        int entero2 = 4;
+
+        if (entero2/2==0){
+        System.out.println("El segundo numero es par");
+        }
+        else{
+        System.out.println("El segundo numero no es par");
+        }
+    }
+}
