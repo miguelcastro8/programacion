1667a77fa494d59234d9c97338edfcd26419e259
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-21 18:50:30 +0200

    ejercicio3 Tema2 Tarea6 java

--- UNIDAD2/TAREA6/src/main/java/eejrcicio3.java
+++ UNIDAD2/TAREA6/src/main/java/eejrcicio3.java
@@ -3,6 +3,15 @@ class ejercicio3 {
 
     public static void main( String args[] ) {
 
-        System.out.println("introduce un numero");
+        System.out.println("introduce un numero entero: ");
         Scanner teclado = new Scanner(System.in);
         int numero = teclado.nextInt();
+
+        if (numero % 1 == 0) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+        }
+    }
