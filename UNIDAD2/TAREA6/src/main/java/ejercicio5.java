cab42e5d29b2b8afc77d111ed26e189abe3fb01e
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-22 17:01:17 +0200

    ejercicio5 Tema2 Tarea6 java

--- UNIDAD2/TAREA6/src/main/java/ejercicio5.java
+++ UNIDAD2/TAREA6/src/main/java/ejercicio5.java
@@ -3,13 +3,30 @@ class ejercicio5 {
 
     public static void main( String args[] ) {
 
-        System.out.println("introduce un numero");
         Scanner teclado = new Scanner(System.in);
+        System.out.println("introduce un radio");
+        int radio = teclado.nextInt();
+        System.out.println("introduce un numero");
         int numero = teclado.nextInt();
+        int pi = 3;
+
+        if (numero == 1);{
+            System.out.println("El diametro es: " + 2 * radio);
+        }
+
+        if (numero == 2);{
+            System.out.println("El perimetro es: " + 2 * pi * radio);
+        }
+
+        if (numero == 3);{
+            System.out.println("El area es: " + pi * radio * radio);
+        }
+
         switch(numero) {
             case 1: System.out.println( "calcular diametro" ); break;
             case 2: System.out.println( "calcular perimetro" ); break;
             case 3: System.out.println( "calcular area" ); break;
+            default: System.out.println("caso desconocido"); break;
         }
     }
 }
