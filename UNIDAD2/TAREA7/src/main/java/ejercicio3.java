53ebbfd8bcf6af282cb18462d53980782f2fc916
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-22 18:27:37 +0200

    ejercicio3 tarea7 tema2 java

---
+++ UNIDAD2/TAREA7/src/main/java/ejercicio3.java
@@ -0,0 +1,9 @@
+class ejercicio3 {
+
+    public static void main( String[] args ) {
+
+        int numero = (int) (Math.random() * 25 + 97);
+        char minuscula_aleatoria = (char) numero;
+        System.out.println("numero es: " + numero "minuscula aleatoria es: " + minuscula_aleatoria);
+    }
+}
