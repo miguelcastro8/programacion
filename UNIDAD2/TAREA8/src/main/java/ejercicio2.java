498aa6a01322065b7c2021178dcf5fb6d4a1045a
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-23 12:48:16 +0200

    ejercicio2 tema2 tarea8 java

---
+++ UNIDAD2/TAREA8/src/main/java/ejercicio2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class ejercicio2 {
+
+    public static void main( String[] args ) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce una edad: ");
+        int edad = teclado.nextInt();
+        int suma_edad = edad++;
+
+        while (edad > 0) {
+            System.out.print("muy bien, vuelve a introducir otra edad: ");
+            edad = teclado.nextInt();
+        }
+        System.out.println("fin!");
+        System.out.println("La suma de las edades es: " + suma_edad);
+    }
+}
