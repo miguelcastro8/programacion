67f874958301c3988f4140d2e275dc28a3973d07
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-23 16:20:45 +0200

    ejercicio11 tema2 tarea8 java

---
+++ UNIDAD2/TAREA8/src/main/java/ejercicio11.java
@@ -0,0 +1,13 @@
+public class ejercicio11 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabla de multiplicar del " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i  + " X " + j + " =" + i*j);
+            }
+
+        }
+
+    }
+}
