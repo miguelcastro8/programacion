30c28fc1a2f328dcc8145b59abfeb052d82b5199
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-25 17:17:43 +0100

    ejercicio4 tarea9 unidad2 java

---
+++ UNIDAD2/TAREA9/src/main/java/ejercicio4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class ejercicio4 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca una frase: ");
+        String s1 = teclado.nextLine();
+        System.out.println("¿Es el caracter central eun espacio?");
+    }
+}
