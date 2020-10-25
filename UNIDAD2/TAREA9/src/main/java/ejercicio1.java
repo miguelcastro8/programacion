95954c7cdcfa226d0a593e16a5e9df8628cab240
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-25 14:21:47 +0100

    ejercicio1 tarea9 unidad2 java

---
+++ UNIDAD2/TAREA9/src/main/java/ejercicio1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class ejercicio1 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        String s1 = "hola";
+        String s2 = "adios";
+
+        System.out.println("La longitud de la primara palabra es: " + s1.length());
+        System.out.println("La longitud de la segunda palabra es: " + s2.length());
+    }
+}
