e3bb0e3f678efbead7fb83ae524940b0687df297
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-25 16:45:42 +0100

    ejercicio3 tarea9 unidad2 java

---
+++ UNIDAD2/TAREA9/src/main/java/ejercicio3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class ejercicio3 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca una contraseña: ");
+        String s1 = teclado.nextLine();
+
+
+        System.out.println("Intenta adivinar la contraseña: ");
+        System.out.println("Su primera letra es: " + s1.charAt(0));
+        System.out.println("Su ultima letra es: " + s1.charAt(s1.length()-1));
+        System.out.println("Su longitud es: " + s1.length());
+        System.out.println("Intenta adivinarla: ");
+        String s2 = teclado.nextLine();
+        if (s1.equals(s2)){
+            System.out.println("Acertaste");
+        }
+        else {
+            System.out.println("Comprobandolas alfabeticamente podemos ver que: " + s1.compareTo(s2) + "alfabeticamente");
+        }
+
+    }
+}
