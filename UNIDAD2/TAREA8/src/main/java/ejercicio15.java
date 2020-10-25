19d8a68a5ba716f07fc8ee504ebe43e2a590e378
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-10-25 13:56:27 +0100

    ejercicio15 tema2 tarea8 java

---
+++ UNIDAD2/TAREA8/src/main/java/ejercicio15.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class ejercicio15 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("inrtoduce un numero: ");
+        int numero = teclado.nextInt();
+    }
+}
