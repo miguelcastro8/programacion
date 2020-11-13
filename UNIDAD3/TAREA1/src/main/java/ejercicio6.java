09e3ffad502ba60f92a0901549b80674093c84bc
miguelcastro8 <72299502+miguelcastro8@users.noreply.github.com>
2020-11-13 16:11:05 +0100

    unidad 3 java

---
+++ UNIDAD3/TAREA1/src/main/java/ejercicio6.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class ejercicio6{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un caracter:");
+        String vocal = sc.nextLine();
+
+        if (vocal.contains("a")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("e")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("i")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("o")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("u")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("A")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("E")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("I")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("O")){
+            System.out.println("Es una vocal");
+        }
+        if (vocal.contains("U")){
+            System.out.println("Es una vocal");
+        }
+    }
+}
