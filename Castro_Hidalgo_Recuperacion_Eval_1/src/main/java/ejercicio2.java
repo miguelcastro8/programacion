c6b898fea1e54329bff7f33ba1ef32de081ef469
miguelcastro8 <alum.mcastroh@iesalixar.org>
2021-01-11 15:57:47 +0100

    recuperacion

---
+++ Castro_Hidalgo_Recuperacion_Eval_1/src/main/java/ejercicio2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class ejercicio2{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce una alura: ");
+        int altura = sc.nextInt();
+        System.out.println("Introduce una anchura: ");
+        int anchura = sc.nextInt();
+
+        for (int i=1; i<=altura; i++){
+            for (int j=1; j<=anchura; j++){
+
+            }
+        }
+    }
+}
